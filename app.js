b59b3ddfea7b1a881dd1c35547be39d2cfab17c0
idrees khan <midreeskhan7276@gmail.com>
2023-09-15 18:39:30 +0500

    done

---
+++ app.js
@@ -0,0 +1,215 @@
+// alert("hellooo worldddd")
+
+
+                   ///////////      chapter 12 to 13    //////////////////
+
+
+///// question  = 1    //////////
+
+// var character = prompt('Enter number');
+// if (character == character.toUpperCase()) {
+//  alert ('upper case true');
+// }
+// if (character == character.toLowerCase()){
+//  alert ('lower case true');
+// }
+
+
+///// question  = 2    //////////
+
+
+// var int1=+prompt("Enter number")
+// var int2=+prompt("Enter number")
+
+// if (int1<int2) {
+//     alert("Greater Than "+int2)
+// }
+// if (int1>int2) {
+//     alert("Greater Than "+int1)
+// }
+
+
+        ///// question  = 3    ////////// 
+
+// var user =+prompt("Enter Number")
+// var oper=prompt("Enter operator")
+// if (oper=="-") {
+//     alert("Negative")
+// }
+// if (oper=="+") {
+//     alert("Positive")
+// }
+// if (user=="0") {
+//     alert("zero")
+// }
+
+///// question  = 4    //////////
+
+// var char= +prompt("Enter");
+// if (char==1) {
+//     alert("1")
+// }
+// else{
+//     alert("OtherWise")
+// }
+
+
+///// question  = 5    //////////
+ 
+
+
+// var pas="idrees khan"
+// var pr=prompt("Enter Password")
+
+// if(pr==" "){
+// alert("please enter Password")
+// }
+// if (pr==pas) {
+//     alert("Correct!")
+// }
+// else{
+//     alert("Please enter correct password")
+// }
+
+
+    ///// question  = 6    //////////
+
+
+// var hour=+prompt("Enter Hour");
+// if(hour<18){
+// alert("Good Day")
+// }
+// else{
+//     alert("Good Evening")
+// }
+
+
+
+// var hour=+prompt("Enter Hour")
+// if(hour=="13"){
+//     alert("1 PM")
+// }
+// if(hour=="14"){
+//     alert("2 PM")
+// }if(hour=="15"){
+//     alert("3 PM")
+// }if(hour=="16"){
+//     alert("4 PM")
+// }if(hour=="17"){
+//     alert("5 PM")
+// }if(hour=="18"){
+//     alert("6 PM")
+// }if(hour=="19"){
+//     alert("7 PM")
+// }if(hour=="20"){
+//     alert("8 PM")
+// }if(hour=="21"){
+//     alert("9 PM")
+// }
+// if(hour=="22"){
+//     alert("10 PM")
+// }if(hour=="23"){
+//     alert("11 PM")
+// }if(hour=="24"){
+//     alert("12 PM")
+// }
+
+
+
+
+
+
+  //    CHAPTER = Array   //
+
+// Question  NO 1//
+
+// var stud = [ ];
+// stud[0]="jawan";
+// console.log(stud)
+
+// Question  NO 2//
+
+// var stud = [
+//     {Name : "idrees", Age : 21}
+//  ]
+// console.log(stud)
+
+// Question  NO 3//
+
+// var name1 =[
+//     "bilal","Idrees khan","Abdullah khan"
+// ]
+// console.log(name1)
+
+// Question  NO 4 //
+
+// var num1 =[
+//     41,52,150
+// ]
+// console.log(num1)
+ 
+// Question  NO 5 //
+
+// var user = prompt("enter city name")
+// var cites =["karachi","lahore","multan"]
+// if(user === cites[0]){
+//     document.write("welcome" )
+// }
+// else{document.write("sorry")}
+
+// Question  NO 6 //
+
+// nhi aya samaj
+
+// Question  NO 7 //
+ 
+
+// var arr = ["SSC", "HSC", "BCS",
+//     "BS", "BCOM", "MS", "M.Phil", "PhD"]
+//     document.write("1)" + arr[0]+ "<br>"+ "2)" + arr[1]+ "<br>"+ "3)" + arr[2]+ "<br>"
+//     + "4)" + arr[3]+ "<br>"+ "5)" + arr[4]+ "<br>"+ "6)" + arr[5]+ "<br>"
+//     + "7)" + arr[6]+ "<br>"+ "8)" + arr[7]+ "<br>")
+
+// Question  NO 8 //
+
+// var names =["bilal","Idrees","Abdullah"]
+// var score =[320,280,270]
+// document.write("score of "+names[0]+".percentage is :67%"+"<br>"+"score of "+names[1]+".percentage is :64%"+"<br>"+"score of "+names[2]+".percentage is :57%")
+  
+
+ // Question  NO 9 //
+
+// smjh ni araha
+
+
+// Question  NO 10//
+
+// var list =[850,250,200,522,250,500] 
+// document.write(list + "<br>"+ "<br>")
+// var list =[850,250,200,522,250,500]
+// list.sort()
+// document.write("updated " + list)
+
+
+
+    // Question  NO 11//
+
+
+// var cites =["karachi","lahore","multan" , "faislabd"]
+// document.write(cites+ " <br>" + " <br>" )
+// var cites =["karachi","lahore","multan" , "faislabd"]
+// var up = cites.slice(1,3)
+// document.write("updated = " + up)
+
+
+ // Question  NO 12 //
+
+//  var arr = [" this " , " is " , " my " ," cat "]
+//  document.write(arr + "<br>" + "<br>")
+
+// var arr = [" this " , " is " , " my " ," cat "]
+// var joi = arr.join(" " )
+// document.write("updated " + joi)
+
+
+// sir baki sawal samjh ni arahe
